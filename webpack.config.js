6602d70e32d5dc64ab5ef71d244edfae2019fc57
mariosknl <mariosknl@outlook.com>
2020-07-17 18:48:21 +0300

    Webpack configuration created

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: 'raw-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
+    }),
+  ],
+};
