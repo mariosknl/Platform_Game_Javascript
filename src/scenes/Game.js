a7daaf796877e61dc75ad1b333361471a22e553e
mariosknl <mariosknl@outlook.com>
2020-07-19 09:31:27 +0300

    collider added

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -27,6 +27,8 @@ export default class Game extends Phaser.Scene {
       body.updateFromGameObject();
     }
 
-    this.physics.add.sprite(240, 320, 'bunny-stand');
+    const player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5);
+
+    this.physics.add.collider(platforms, player);
   }
 }
