f676bce7fb6ecb53df4c49caf924afd5687c8837
mariosknl <mariosknl@outlook.com>
2020-07-23 21:27:36 +0300

    sound fixed

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -19,13 +19,5 @@ export default class TitleScene extends Phaser.Scene {
       'grey_button1', 'grey_button2', 'Credits', 'Credits');
 
     this.rankingBtn = new Button(this, config.width / 2, config.height / 2 + 200, 'grey_button1', 'grey_button2', 'Ranking', 'ranking');
-
-    this.model = this.sys.game.globals.model;
-    if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
-      this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
-      this.bgMusic.play();
-      this.model.bgMusicPlaying = true;
-      this.sys.game.globals.bgMusic = this.bgMusic;
-    }
   }
 }
