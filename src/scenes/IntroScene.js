22704fe41e60f8aea791a36130e1d476a845eb67
mariosknl <mariosknl@outlook.com>
2020-07-23 21:25:18 +0300

    sound fixed

--- src/scenes/IntroScene.js
+++ src/scenes/IntroScene.js
@@ -42,8 +42,13 @@ export default class IntroScene extends Phaser.Scene {
       }),
     });
 
-    this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
-    this.bgMusic.play();
+    this.model = this.sys.game.globals.model;
+    if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
+      this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
+      this.bgMusic.play();
+      this.model.bgMusicPlaying = true;
+      this.sys.game.globals.bgMusic = this.bgMusic;
+    }
   }
 
   update() {
