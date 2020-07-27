416b4fb3325f6e1cfcbcf9096da3e77d000400b4
mariosknl <mariosknl@outlook.com>
2020-07-27 15:33:47 +0300

    Fixed the wrong test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -3,17 +3,24 @@ import bgImage from '../src/Objects/background';
 import Dialogue from '../src/scenes/Dialogue';
 import scenario from '../src/Objects/dialog';
 
-
 const { dialog } = scenario;
 const { background } = bgImage;
 
 it('creates a new scene', () => {
-  const scene = new GameScene('First', bgImage.background[0], 'move', 'Dialog1');
+  const scene = new GameScene(
+    'First',
+    bgImage.background[0],
+    'move',
+    'Dialog1',
+  );
   expect(scene).toBeTruthy();
 });
 
 it('creates a new dialog', () => {
-  const newDialogue = new Dialogue('Dialog1', new Dialogue('Dialog1', 'Stage 1 Cleared', scenario.dialog[0][0], 'Second'));
+  const newDialogue = new Dialogue(
+    'Dialog1',
+    new Dialogue('Dialog1', 'Stage 1 Cleared', scenario.dialog[0][0], 'Second'),
+  );
   expect(newDialogue).toBeTruthy();
 });
 
@@ -22,7 +29,7 @@ it('dialogues imported from array', () => {
 });
 
 it('dialogues are an array of arrays', () => {
-  expect(Array.isArray(dialog[0])).toBeTruthy();
+  expect(Array.isArray(dialog)).toBeTruthy();
 });
 
 it('gets the backgound image from an array', () => {
